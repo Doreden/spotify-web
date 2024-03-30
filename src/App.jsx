76e98783aa4a516f9b828969c42b5fbebc902b0a
Ordoga <ordogga@gmail.com>
2024-03-30 17:38:33 +0300

    Routes setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Route, HashRouter as Router, Routes } from 'react-router-dom'
 
 // Pages
 import { Homepage } from './pages/Homepage.jsx'
+import { Station } from './pages/Station.jsx'
+import { Search } from './pages/Search.jsx'
+import { Artist } from './pages/Artist.jsx'
+import { Genre } from './pages/Genre.jsx'
 
 // Components
 import { Sidebar } from './cmps/Sidebar.jsx'
@@ -19,9 +23,11 @@ function App() {
         <Sidebar/>
         <Routes>
           <Route path="/" element={<Homepage/>} />
+          <Route path="/search" element={<Search/>} />
+          <Route path="/artist/:id" element={<Artist/>} />
+          <Route path="/playlist/:id" element={<Station/>} />
+          <Route path="/genre/:id" element={<Genre/>} />
         </Routes>
-
-
       </Router>
 
     </>
