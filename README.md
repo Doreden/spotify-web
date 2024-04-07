855886ef948c474a2a12693b7525a18f683ab155
Doreden <dor.edenm@gmail.com>
2024-04-07 19:42:49 +0300

    Edit grid layout templates, edit sidebar grid, create readme

--- README.md
+++ README.md
@@ -2,11 +2,27 @@
 
 > Our version for the beloved music player and streaming services
 
+## Installation
+
+## GIT
+
+> git clone
+> git clone
+
 ## Features
 
-> features list...
+> features list.. .
 
-### Plugins used:
+ ## Npm Libaries
+ - react-router
+ - react-icon
+ - react-svg
+
+
+
+ ### Plugins used:
+
+- 
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/cmps/sidebar/Navigation.jsx
+++ src/cmps/sidebar/Navigation.jsx
@@ -1,18 +1,22 @@
-import { Link } from "react-router-dom"
-
-export function Navigation(){
-    return (
-        <>
-            <div className="navigation">
-                <ul className="nav-buttons">
-                    <li className="nav-button">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="nav-button">
-                        <Link to="/search">Search</Link>
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
+import { Link } from "react-router-dom";
+import homeIcon from "../../assets/imgs/home1.svg";
+import { ReactSVG } from "react-svg";
+export function Navigation() {
+  return (
+    <>
+      <div className="navigation">
+        <ul>
+          <li className="nav-button">
+            <a href="/">
+              <ReactSVG className="icon" src={homeIcon} /> 
+              <span>Home</span>
+            </a>
+          </li>
+          <li className="nav-button">
+            <Link to="/search">Search</Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
