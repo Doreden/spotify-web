d954b1393af6268c96d363b96afac0521aa4999d
liron28 <liron.d28@gmail.com>
2024-05-27 20:41:01 +0300

    Checking the scss

--- src/cmps/sidebar/SidebarLibary.jsx
+++ src/cmps/sidebar/SidebarLibary.jsx
@@ -1,12 +1,43 @@
 import { SidebarLibaryHeader } from "./SidebarLibaryHeader.jsx";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { StationPreview } from "./StationPreview.jsx";
 import { OptionsModal } from "../OptionsModal.jsx";
+import { useEffect, useState } from "react";
+import { ContextMenu } from "./ContextMenu.jsx";
 
 export function SidebarLibary() {
+  // const [station, setStation] = useState([])
+  const [isActiveId, setIsActiveId] = useState(null)
+  const [contextMenu, setContextMenu] = useState(null)
   const stations = useSelector(
     (storeState) => storeState.stationModule.stations
   );
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    document.addEventListener('click', handleCloseContextMenu)
+    return () => {
+      document.removeEventListener('click', handleCloseContextMenu)
+    }
+  }, [])
+
+
+  const handleContextMenu = (event, station) => {
+    event.preventDefault()
+    setContextMenu({
+      isVisible: true,
+      x: event.clientX,
+      y: event.clientY,
+      station: station,
+    })
+  }
+
+  const handleCloseContextMenu = () => {
+    setContextMenu(null)
+  }
+  const handleStationClick = (id) => {
+    setIsActiveId(id)
+  }
 
   return (
     <>
@@ -15,11 +46,22 @@ export function SidebarLibary() {
 
         <div className="libary-station-list">
           {stations.map((station) => (
-            <div key={station.id} className="preview-item">
-              <StationPreview station={station} />
+            <div key={station.id} className="preview-item" onContextMenu={(event) => handleContextMenu(event, station)} >
+              <StationPreview station={station} 
+              isActiveId={station.id === isActiveId}
+              onClick={handleStationClick} />
             </div>
           ))}
         </div>
+        {contextMenu && (<ContextMenu
+          x={contextMenu.x}
+          y={contextMenu.y}
+          isActiveId={isActiveId}
+          onEdit={() => handleEditStation(contextMenu.station.id)}
+          onRemove={() => handleRemoveStation(contextMenu.station.id)}
+          onAdd={() => onAddStation()}
+
+      />) }
       </div>
     </>
   );
--- src/cmps/sidebar/StationPreview.jsx
+++ src/cmps/sidebar/StationPreview.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 
-export function StationPreview({ station }){
+export function StationPreview({ station, isActiveId, onClick  }){
+    const handleClick = (e) => {
+        e.stopPropagation()
+        onClick(station._id)
+      }
 
     return (
-        <Link to={`/station/${station.id}`}>
-            <div className="station-preview">
+        <Link to={`/station/${station.id}`} onClick={handleClick} >
+            <div className="station-preview" onClick={() => onClick(station.id)}>
                 <img src={station.albumCoverUrl}></img>
                 <div className="station-preview-text">
                     <div className="station-name">{station.name}</div>
