5f50c79df19ec603d6c99d844b6768277e713210
Ordoga <ordogga@gmail.com>
2024-05-31 11:52:15 +0300

    Create Station, Delete Station Actions built to update station db, user db and store - causes re-render

--- src/cmps/sidebar/SidebarLibaryHeader.jsx
+++ src/cmps/sidebar/SidebarLibaryHeader.jsx
@@ -1,32 +1,13 @@
-import { addMiniStationToUser } from '../../store/actions/user.action'
+import { createNewStationByUser } from '../../store/actions/user.action'
 
 import { ReactSVG } from "react-svg"
-
 import Libary from '../../assets/imgs/libary.svg'
 import Plus from '../../assets/imgs/plus.svg'
-import { stationService } from '../../services/station.service'
-import { UserService } from '../../services/user.service'
 
 export function SidebarLibaryHeader({ loggedInUser }) {
 
-
     async function handleAddStation() {
-        // TOASK - Convert to Action?
-        try {
-            const { id, fullname, imgUrl } = loggedInUser
-            const formattedUser = { id, fullname, imgUrl }
-            // Creates new station in database
-            const newStation = await stationService.createNewStation(formattedUser)
-            const miniNewStation = stationService.convertToMiniStation(newStation)
-            // Add new station to user in database
-            UserService.addMiniStation(miniNewStation)
-            // Add new station in store to re-render
-            addMiniStationToUser(miniNewStation)
-        } catch (err) {
-            console.log(err)
-        }
-
-        // Add mini station to user
+        await createNewStationByUser(loggedInUser)
     }
 
     return (
--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -1,9 +1,5 @@
-import { UserService } from "../../services/user.service"
-
-
 const initialState = { user: null }
 
-export const LOAD_USER = 'LOAD_USER'
 export const LOAD_STATIONS = 'LOAD_STATIONS'
 export const ADD_STATION = 'ADD_STATION'
 export const REMOVE_STATION = 'REMOVE_STATION'
@@ -11,16 +7,15 @@ export const SET_USER = 'SET_USER'
 
 export function userReducer(state = initialState, cmd){
     switch(cmd.type){
-        case LOAD_USER:
-            return {...state, user: UserService.createMinimalUser(cmd.user)}
+
         case SET_USER:
             return {...state, user: cmd.user}
         case LOAD_STATIONS:
             return {...state, stations: cmd.newStations}
         case ADD_STATION:
-            return {...state, user : {...state.user, likedStations: [...state.user.likedStations, cmd.newStation] } }
+            return {...state, user : {...state.user, likedStations: [...state.user.likedStations, cmd.miniNewStation] } }
         case REMOVE_STATION:
-            return {...state, stations: state.stations.filter(station => station.id !== cmd.stationId)}
+            return {...state, user : {...state.user, likedStations : state.user.likedStations.filter(station => station.id !== cmd.stationId)} }
        
         default:
             return state
