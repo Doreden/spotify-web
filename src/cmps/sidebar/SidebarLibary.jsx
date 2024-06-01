bcbcb7d0a002625521ce966633ef9636c5526797
Ordoga <ordogga@gmail.com>
2024-06-01 13:04:37 +0300

    Context Menu Design

--- src/cmps/sidebar/SidebarLibary.jsx
+++ src/cmps/sidebar/SidebarLibary.jsx
@@ -1,18 +1,18 @@
-import { SidebarLibaryHeader } from "./SidebarLibaryHeader.jsx";
-import { useDispatch, useSelector } from "react-redux";
-import { StationPreview } from "./StationPreview.jsx";
-import { OptionsModal } from "../OptionsModal.jsx";
-import { useEffect, useState } from "react";
-import { ContextMenu } from "./ContextMenu.jsx";
+import { SidebarLibaryHeader } from "./SidebarLibaryHeader.jsx"
+import { useDispatch, useSelector } from "react-redux"
+import { StationPreview } from "./StationPreview.jsx"
+import { OptionsModal } from "../OptionsModal.jsx"
+import { useEffect, useState } from "react"
+import { ContextMenu } from "./ContextMenu.jsx"
 
 export function SidebarLibary() {
   const [isActiveId, setIsActiveId] = useState(null)
   const [contextMenu, setContextMenu] = useState(null)
   const loggedInUser = useSelector((storeState) => storeState.userModule.user)
   console.log(loggedInUser)
- 
+
   const miniStations = loggedInUser ? loggedInUser.likedStations : null
-  
+
   console.log(miniStations)
   useEffect(() => {
     document.addEventListener('click', handleCloseContextMenu)
@@ -23,20 +23,20 @@ export function SidebarLibary() {
 
 
   const handleContextMenu = (event, station) => {
-    // event.preventDefault()
-    // setContextMenu({
-    //   isVisible: true,
-    //   x: event.clientX,
-    //   y: event.clientY,
-    //   station: station,
-    // })
+    event.preventDefault()
+    setContextMenu({
+      isVisible: true,
+      x: event.clientX,
+      y: event.clientY,
+      station: station,
+    })
   }
 
   const handleCloseContextMenu = () => {
-    // setContextMenu(null)
+    setContextMenu(null)
   }
   const handleStationClick = (id) => {
-    // setIsActiveId(id)
+    setIsActiveId(id)
   }
   if (!loggedInUser) {
     return "Log in to create and share playlists"
@@ -54,13 +54,13 @@ export function SidebarLibary() {
         <div className="libary-station-list">
           {miniStations.map((station) => (
             <div key={station.id} className="preview-item" onContextMenu={(event) => handleContextMenu(event, station)} >
-              <StationPreview station={station} 
-              isActiveId={station.id === isActiveId}
-              onClick={handleStationClick} />
+              <StationPreview station={station}
+                isActiveId={station.id === isActiveId}
+                onClick={handleStationClick} />
             </div>
           ))}
         </div>
-        {/* {contextMenu && (<ContextMenu
+        {contextMenu && (<ContextMenu
           x={contextMenu.x}
           y={contextMenu.y}
           isActiveId={isActiveId}
@@ -68,7 +68,7 @@ export function SidebarLibary() {
           onRemove={() => handleRemoveStation(contextMenu.station.id)}
           onAdd={() => onAddStation()}
 
-      />) } */}
+        />)}
       </div>
     </>
   )
